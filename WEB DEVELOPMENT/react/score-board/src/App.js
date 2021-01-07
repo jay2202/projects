0a8fc565c0feb30827fe8f8883256750a2a1561e
Mritunjay Kumar <mritu.ab0509@gmail.com>
2021-01-07 23:17:18 +0530

    changes

---
+++ WEB DEVELOPMENT/react/score-board/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Head from './Head'
+import Match from './Match'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Head></Head>
+      {/* <Match/> */}
+    </div>
+  );
+}
+
+export default App;
